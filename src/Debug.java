97ce666636d6c554073bd371fb2a6f705308b916
Taimur <tsohrab8@gmail.com>
2017-06-19 10:24:45 -0400

    first commit

---
+++ src/Debug.java
@@ -0,0 +1,13 @@
+
+public class Debug {
+	 public static void main(String[] args) {
+		 double x, y;
+		 
+		 x = 3.1415;
+		 y = 3.64;
+		 
+		 System.out.println("pi is approximately " + x);
+		 System.out.println("My GPA was " + y);
+		 }
+
+}
